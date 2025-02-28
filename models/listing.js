70c40e113620a4b18f6a3bb6ca8c86b40df46877
Dheemanth.N.J <011vpu2dheemanthnj@gmail.com>
2025-03-01 02:55:21 +0530

    Initial commit - Added MERN project files

---
+++ models/listing.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js"); 
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    url: {
+      type: String,
+      required: true,
+      default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    },
+    filename: {
+      type: String,
+      required: true
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: [0, "Price must be positive"]
+  },
+ 
+  location: 
+  {
+    type: { type: String, default: "Point"},
+    coordinates: { type: [Number]}
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+});
+
+// Middleware to delete reviews when a listing is deleted
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    try {
+      await Review.deleteMany({ _id: { $in: listing.reviews } });
+    } catch (error) {
+      console.error("Error deleting associated reviews:", error);
+    }
+  }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
